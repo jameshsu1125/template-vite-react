ca1c75ed585543695b23d9c9300d358acdb84ba9
jameshsu1125 <jameshsu1125@gmail.com>
2023-10-05 16:23:04 +0800

    adjustment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	root: resolve(__dirname, 'src/pages'),
+	build: {
+		outDir: resolve(__dirname, 'dist'),
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, 'src/pages/index.html'),
+			},
+		},
+	},
 	css: {
 		preprocessorOptions: {
 			less: {
@@ -14,23 +23,5 @@ export default defineConfig({
 			},
 		},
 	},
-	plugins: [
-		react(),
-		createHtmlPlugin({
-			minify: true,
-			pages: [
-				{
-					entry: 'src/pages/index.tsx',
-					filename: 'index.html',
-					template: '/template/index.html',
-					injectOptions: {
-						data: {
-							title: 'index',
-							injectScript: `<script src="./index.js"></script>`,
-						},
-					},
-				},
-			],
-		}),
-	],
+	plugins: [react()],
 });
