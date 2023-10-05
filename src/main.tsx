2e66c5cc17b0c24019923a855d6697d0fcb344de
jameshsu1125 <jameshsu1125@gmail.com>
2023-10-05 14:39:08 +0800

    add env

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+console.log(import.meta.env);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
