58f6ff786c8c6365b787bde3a26cd7bdb602bae6
jameshsu1125 <jameshsu1125@gmail.com>
2023-10-24 19:27:23 +0800

    upgrade msw

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,18 +1,21 @@
 import { REST_PATH } from '@/settings/config';
 import { faker } from '@faker-js/faker';
 import { mergePath } from 'lesca-fetcher';
-import { rest } from 'msw';
+import { HttpResponse, http } from 'msw';
 
 export const handlers = [
-  rest.get(mergePath(REST_PATH.test), (_, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        userId: faker.string.uuid(),
-        id: faker.number.int(10),
-        title: faker.lorem.lines(1),
-        completed: faker.datatype.boolean(),
-      }),
-    );
+  http.get(mergePath(REST_PATH.test), () => {
+    return HttpResponse.json({
+      userId: faker.string.uuid(),
+      id: faker.number.int(10),
+      title: faker.lorem.lines(1),
+      completed: faker.datatype.boolean(),
+    });
+  }),
+  http.get('/api', () => {
+    return new HttpResponse(null, {
+      status: 404,
+      statusText: 'Out Of Service',
+    });
   }),
 ];
