f56fda51aef89fd68dad329ae28692e08840158d
jameshsu1125 <jameshsu1125@gmail.com>
2023-10-31 18:54:16 +0800

    revert to no netlify

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { env } from 'process';
-
-const app = new express();
-app.get('/api/hi', (_, res) => {
-  res.send('hello');
-});
-
-app.listen(env.port, () => console.log(`server is served. port = ${env.port}`));
