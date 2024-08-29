77a78f0e6de841112b075c2ca261ce0127c1ff5f
jameshsu1125 <jameshsu1125@gmail.com>
2024-08-29 14:58:57 +0800

    chore: Remove unused code and update button component usage in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,6 @@
 import Button from '@/components/button';
 import useTodos from '@/hooks/useTodos';
-import { IReactProps, TransitionType } from '@/settings/type';
-import OnloadProvider from 'lesca-react-onload';
+import { IReactProps } from '@/settings/type';
 import { memo, useState } from 'react';
 import { HomeContext, HomeState, THomeState } from './config';
 import './index.less';
@@ -9,20 +8,17 @@ import './index.less';
 const Home = memo(({ children }: IReactProps) => {
   const [state, setState] = useState<THomeState>(HomeState);
   const [todos, getTodos] = useTodos();
-  const [transition, setTransition] = useState(TransitionType.Unset);
 
   return (
-    <OnloadProvider onload={() => setTransition(TransitionType.FadeIn)}>
-      <div className='Home'>
-        <HomeContext.Provider value={[state, setState]}>
-          <h1 className='text-2xl'>{children}</h1>
-          <Button transition={transition} onClick={getTodos}>
-            <Button.regular>Fetch</Button.regular>
-          </Button>
-          <p className='text-center'>{JSON.stringify(todos)}</p>
-        </HomeContext.Provider>
-      </div>
-    </OnloadProvider>
+    <div className='Home'>
+      <HomeContext.Provider value={[state, setState]}>
+        <h1 className='text-2xl'>{children}</h1>
+        <Button onClick={getTodos}>
+          <Button.regular>Fetch</Button.regular>
+        </Button>
+        <p className='text-center'>{JSON.stringify(todos)}</p>
+      </HomeContext.Provider>
+    </div>
   );
 });
 
