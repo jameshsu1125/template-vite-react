5a0c8f750f5b4e3977e3c4434bc413be19bd207d
James Hsu <jameshsu1125@gmail.com>
2025-08-12 18:24:54 +0800

    Update regular.tsx

--- src/components/button/regular.tsx
+++ src/components/button/regular.tsx
@@ -2,6 +2,8 @@ import { IReactProps } from '@/settings/type';
 import { memo } from 'react';
 
 const Regular = memo(({ children }: IReactProps) => (
-  <div className='bg-secondary text-primary w-full rounded-xl p-3'>{children}</div>
+  <div className='bg-secondary text-primary pointer-events-none w-full rounded-xl p-3'>
+    {children}
+  </div>
 ));
 export default Regular;
