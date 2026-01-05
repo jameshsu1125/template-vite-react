1efd32f620dade19d3e06cf66a1d933ab5933e93
jameshsu1125 <jameshsu1125@gmail.com>
2026-01-05 10:55:06 +0800

    chore: 更新 @faker-js/faker、react、react-dom、@tailwindcss/postcss、@types/react、@typescript-eslint/eslint-plugin、@typescript-eslint/parser、@vitejs/plugin-react、eslint、eslint-plugin-react-refresh、msw、prettier-plugin-tailwindcss、tailwindcss 和 vite 依賴版本，並更新 mockServiceWorker.js 的版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import react from '@vitejs/plugin-react';
-import { certificateFor } from 'devcert';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import mkcert from 'vite-plugin-mkcert';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, './src/pages');
-  const { key, cert } = await certificateFor('localhost');
 
   return {
     base: './',
@@ -43,6 +45,7 @@ export default defineConfig(async ({ mode }) => {
           },
         },
       }),
+      mkcert(),
     ],
     resolve: {
       alias: {
@@ -52,7 +55,6 @@ export default defineConfig(async ({ mode }) => {
     server: {
       open: true,
       port: 5173,
-      https: { key, cert },
       proxy: {
         '/api': 'http://localhost:3000',
       },
